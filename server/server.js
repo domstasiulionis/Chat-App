28fe857671d1bd47a3660518eb444215cbdf680b
domstasiulionis <domstasiulionis@gmail.com>
2023-01-07 10:36:09 +0000

    set up a socket connection from server

--- server/server.js
+++ server/server.js
@@ -10,6 +10,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on("connetion", (socket) => {
+  console.log("New connection");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
+});
+
 app.use(router);
 
 server.listen(PORT, () => {
